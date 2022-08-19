dda9d72ba16ec49d9c1507b8872dec8b83df11af
KINPEACE <nikita_1997152@mail.ru>
2022-08-19 23:24:46 +0300

    add Task 38

---
+++ Task38/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+double[] Num = new double[10];
+for(int i = 0; i < Num.Length; i++)
+{
+    Num[i] = new Random().Next(1, 10);
+    Console.Write(Num[i] + " ");
+}
+Console.WriteLine();
+
+double Max = Num[0];
+double Min = Num[0];
+
+for(int i =1; i < Num.Length; i++)
+{
+    if(Max < Num[i])
+    {
+        Max = Num[i];
+    }
+    if(Min > Num[i])
+    {
+        Min = Num[i];
+    }
+}
+
+double Diff = Max - Min;
+Console.WriteLine($"Разница между: Max ({Max}) и Min ({Min}) = {Diff}");
