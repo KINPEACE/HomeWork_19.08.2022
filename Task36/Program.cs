0bec444487e0c4d9775028da2edcdf433a3dc42c
KINPEACE <nikita_1997152@mail.ru>
2022-08-19 23:06:17 +0300

    add Task 36

---
+++ Task36/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. 
+Найдите суму элементов, состоящих на нечетных позициях.*/
+
+Console.Write("Введите колличество элементов массива: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int RandomNumbers(int numbers, int minValue, int maxValue)
+{
+    int[] random = new int[numbers];
+    int summa = 0;
+    Console.Write("Рандомный массив: ");
+    for(int i = 0; i < random.Length; i++)
+    {
+        random[i] = new Random().Next(minValue, maxValue);
+        Console.Write(random[i] + " ");
+        if(i % 2 != 1)
+        {
+            summa = summa + random[i];
+        }
+    }
+    return summa;
+}
+
+int random = RandomNumbers(numbers, 1, 10);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {random}");
